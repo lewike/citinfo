b4d7619388796492c99f912a0f4980ea5000097e
daidongsheng <daids@mail.com>
2020-11-16 20:07:35 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -14,33 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => 'api', 'middleware' => 'api', 'namespace' => 'Api'], function () {
-    Route::get('/weixin', 'WeixinController@check');
-    Route::post('/weixin', 'WeixinController@handle');
-    Route::get('/ip2local/{ip}', 'HelperController@getIpLocal');
-
-    Route::get('/feedback', 'FeedbackController@check');
-    Route::post('/feedback', 'FeedbackController@handle');
-
-    Route::post('/weixin/payment', 'WeixinPaymentController@handle');
-
+Route::group(['prefix' => 'api', 'namespace' => 'Api'], function () {
     Route::get('wechat', 'WechatController@check');
     Route::post('wechat', 'WechatController@handle');
-});
-
-Route::group(['prefix' => 'wx/api', 'namespace' => 'WeixinApi'], function () {
-    Route::get('config', 'ConfigController@index');
-    Route::get('posts', 'PostController@index');
-    Route::get('post/{id}', 'PostController@show');
-    Route::get('category/{id}/posts', 'CategoryPostController@index');
-    Route::get('login', 'UserController@login');
-    Route::post('upload', 'ImageController@upload');
-    Route::post('post/create', 'PostController@create');
-    Route::get('user/posts', 'UserPostController@index');
-    Route::post('user/post/{id}/{action}', 'UserPostController@update');
-});
-
-Route::group(['prefix' => 'wx/api', 'namespace' => 'WeixinApi'], function () {
-    Route::get('topics', 'TopicController@index');
-    Route::get('topic/{topic}', 'TopicController@show');
 });
