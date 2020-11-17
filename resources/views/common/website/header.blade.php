963af664b5c1032b19519a811bfcf97260f55b8c
daidongsheng <daids@mail.com>
2020-11-17 10:14:42 +0800

    update

--- resources/views/common/website/header.blade.php
+++ resources/views/common/website/header.blade.php
@@ -2,7 +2,7 @@
     <div class="topbar row no-gutters border-bottom py-2">
       <div class="col">联系客服：199 0376 3214（微信同号） 周一至周五（早9:00 - 晚5:00） </div> <div class="col text-right"><a href="/about">关于本站</a> | <a href="/contact">联系本站</a> | <a href="/policy">网站声明</a> | <a href="/help">使用帮助</a> | <a class="text-success" href="/post/create"><strong>免费发布信息</strong></a></div>
     </div>
-    <div class="logo py-3"><a href="/" title="在息县网"><img src="/images/zaixixian-logo.png" alt="在息县网" height="64px"></a><a class="d-none" href="/" title="在息县网"><h1>在息县网</h1></a>
+    <div class="logo py-3"><a href="/" title="在息县网"><img src="{{env('CDN_URL')}}/images/zaixixian-logo.png" alt="在息县网" height="64px"></a><a class="d-none" href="/" title="在息县网"><h1>在息县网</h1></a>
     </div>
     <div class="navbar navbar-expand-lg rounded-lg">
     <div class="navbar-nav">
