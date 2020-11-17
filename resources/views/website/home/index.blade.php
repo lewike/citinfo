b1b0ef9eec04e3e1f02f6a96197e801139a76328
daidongsheng <daids@mail.com>
2020-11-17 10:18:10 +0800

    update

--- resources/views/website/home/index.blade.php
+++ resources/views/website/home/index.blade.php
@@ -17,13 +17,13 @@
     </div>
     <ul class="list-unstyled my-4">
       <li class="text-center">
-        <img src="https://img.zaixixian.com/images/ad/ad1.png" alt="">
+        <img src="https://www.zaixixian.com/images/ad/ad1.png" alt="">
       </li>
       <li class="text-center">
-        <img src="https://img.zaixixian.com/images/ad/ad2.png" alt="">
+        <img src="https://www.zaixixian.com/images/ad/ad2.png" alt="">
       </li>
       <li class="text-center">
-        <img src="https://img.zaixixian.com/images/ad/ad3.png" alt="">
+        <img src="https://www.zaixixian.com/images/ad/ad3.png" alt="">
       </li>
     </ul>
   </div>
