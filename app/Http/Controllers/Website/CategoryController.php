b56e557c9fa0f90bb96aafcd4ef28b74452f84fd
daidongsheng <daids@mail.com>
2021-02-24 18:09:37 +0800

    update

--- app/Http/Controllers/Website/CategoryController.php
+++ app/Http/Controllers/Website/CategoryController.php
@@ -12,14 +12,14 @@ class CategoryController extends Controller
     {
         $data['category'] = Category::where('ename', $ename)->first();
         $data['subCategories'] = $data['category']->subCategories()->get();
-        $data['posts'] = $data['category']->posts()->show()->latest('category_stick')->latest('refresh_at')->paginate(40);
+        $data['posts'] = $data['category']->posts()->show()->latest('category_sticky')->latest('refresh_at')->paginate(40);
         return view('website.category.fenlei', $data);
     }
     public function index(Category $category)
     {
         $data['category'] = $category;
         $data['parentCategory'] = $category->parent();
-        $data['posts'] = $category->posts()->show()->latest('category_stick')->latest('refresh_at')->paginate(40);
+        $data['posts'] = $category->posts()->show()->latest('category_sticky')->latest('refresh_at')->paginate(40);
         return view('website.category.index', $data);
     }
 }
