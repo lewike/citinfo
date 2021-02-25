715ea9a4b63ecb7caf7d4ecaeaeedd504a905b86
daidongsheng <daids@mail.com>
2021-02-25 15:25:34 +0800

    update

---
+++ app/Console/Commands/ExpiredCarpool.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Model\Carpool;
+use Illuminate\Console\Command;
+
+class ExpiredCarpool extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'citinfo:expired-carpool';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->expiredStick();
+        $this->expiredCarpool();
+        return 0;
+    }
+
+    public function expiredStick()
+    {
+        Carpool::where('is_sticky', 1)->where('sticky_expired_at', '<', date('Y-m-d H:i:s'))->update(['is_sticky' => 0]);
+    }
+
+    public function expiredCarpool()
+    {
+        Carpool::where('start_at', '<', date('Y-m-d H:i:s'))->delete();
+    }
+}
