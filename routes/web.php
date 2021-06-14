7261e83d7645d5cf437a98b814f0d945b576835e
daidongsheng <daids@mail.com>
2021-06-15 07:58:21 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -114,20 +114,20 @@ Route::group(['namespace' => 'Carpool', 'prefix' => 'pinche'], function(){
 });
 
 Route::group(['namespace' => 'Mobile', 'domain' => 'm.zaixixian.com'], function(){
-        Route::get('/', 'HomeController@index');
-        Route::get('/post/{post}', 'PostController@show');
-        Route::get('/post/views/{post}', 'PostController@views');
-        Route::get('/post/phone/{post}', 'PostController@phone');
-        Route::get('/post/create', 'PostController@create');
+    Route::get('/', 'HomeController@index');
+    Route::get('/post/{post}', 'PostController@show');
+    Route::get('/post/views/{post}', 'PostController@views');
+    Route::get('/post/phone/{post}', 'PostController@phone');
+    Route::get('/post/create', 'PostController@create');
 
-        Route::get('/fenlei/{name}', 'CategoryController@fenlei');
-        Route::get('/category/{category}', 'CategoryController@index');
+    Route::get('/fenlei/{name}', 'CategoryController@fenlei');
+    Route::get('/category/{category}', 'CategoryController@index');
 
-        Route::prefix('wx')->middleware(['wechat.oauth'])->group(function () {
-            Route::get('post/create', 'WechatController@createPost');
-            Route::post('post/create', 'WechatController@savePost');
-            Route::post('post/upload', 'WechatController@upload');
-            Route::post('post/update', 'WechatController@updatePost');
-            Route::get('user', 'WechatController@user');
-        });
+    Route::prefix('wx')->middleware(['wechat.oauth'])->group(function () {
+        Route::get('post/create', 'WechatController@createPost');
+        Route::post('post/create', 'WechatController@savePost');
+        Route::post('post/upload', 'WechatController@upload');
+        Route::post('post/update', 'WechatController@updatePost');
+        Route::get('user', 'WechatController@user');
     });
+});
